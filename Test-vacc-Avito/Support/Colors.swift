bcc109a2be1f03087312da74cf21a0c8602eaa54
Allison <suprun-s@bk.ru>
2024-09-12 23:09:01 +0300

    Support

---
+++ Test-vacc-Avito/Support/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Test-vacc-Avito
+//
+//  Created by Alexander Suprun on 11.09.2024.
+//
+
+import Foundation
+import UIKit
+
+final class Colors {
+    static let settingBackgroundColor: UIColor = UIColor(named: "settingBackgraound")!
+    static let settingTableColor: UIColor = UIColor(named: "settingTable")!
+  }
